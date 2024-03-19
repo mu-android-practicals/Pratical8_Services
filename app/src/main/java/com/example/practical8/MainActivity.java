6996bc10e16014e9d4fe850d17d0634ce97db9b2
GuptaShribhagawat <guptashribhagawat@gmail.com>
2024-03-19 21:54:19 +0530

    Prctical 8

---
+++ app/src/main/java/com/example/practical8/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.practical8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+    public void startService(View view) {
+
+        Intent intent = new Intent(this, MyServices.class);
+        startService(intent);
+    }
+    public void stopService(View view) {
+        Intent intent = new Intent(this, MyServices.class);
+        stopService(intent);
+    }
+
+}
